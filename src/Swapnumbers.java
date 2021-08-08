cf6f7f7569c1ed6f0e6288d38c64d1ca99674d4d
SRC0747 <srcsrk1998@gmail.com>
2021-08-08 20:55:42 +0530

    Swaping two numbers.

---
+++ src/Swapnumbers.java
@@ -0,0 +1,17 @@
+public class Swapnumbers {
+    public static void main(String[] args)
+    {
+        int a=7;
+        int b=47;
+        int temp;
+        System.out.println("Before swapping:");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping:");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
